de7c71f47a0c97c6dabf3254e6e71be698f1d760
goyalsakshi1229 <goyalsakshi1229@gmail.com>
2022-01-22 13:54:11 +0530

    Addditon of two numbers

--- c++ code/Addition.cpp
+++ c++ code/Addition.cpp
@@ -5,6 +5,6 @@ int main()
     int a, b;
     cout << "Enter two numbers :";
     cin >> a >> b;
-    cout << "Adition of two numbers:" << (a + b);
+    cout << "\nAdition of two numbers:" << (a + b);
     return 0;
 }
